0f0b14865dbdc4efd5f6735e097b9bea5b52f690
Chris Hesterman <chrishesterman14@gmail.com>
2021-05-26 15:35:17 -0700

    Delete unused code in App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -2,34 +2,8 @@ import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import axios from 'axios';
 import App from './App';
-import List from './List';
 
 const apiURL = 'https://api.hatchways.io/assessment/students';
-
-const studentProfiles = [
-  {
-    city: 'Fushë-Muhurr',
-    company: 'Yadel',
-    email: 'iorton0@imdb.com',
-    firstName: 'Ingaberg',
-    grades: ['78', '100', '92', '86', '89', '88', '91', '87'],
-    id: '1',
-    lastName: 'Orton',
-    pic: 'https://storage.googleapis.com/hatchways-app.appspot.com/assessments/data/frontend/images/voluptasdictablanditiis.jpg',
-    skill: 'Oracle'
-  },
-  {
-    city: 'Sanghan',
-    company: 'Avamm',
-    email: 'cboards1@weibo.com',
-    firstName: 'Clarke',
-    grades: ['75', '89', '95', '93', '99', '82', '89', '76'],
-    id: '2',
-    lastName: 'Boards',
-    pic: 'https://storage.googleapis.com/hatchways-app.appspot.com/assessments/data/frontend/images/voluptasautreprehenderit.jpg',
-    skill: 'Sports'
-  }
-];
 
 describe('<App />', () => {
   test('correctly fetches API data', () => {
